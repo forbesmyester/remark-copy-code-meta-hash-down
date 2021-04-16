60af2a9c845a72a0f540e531bff648019daf87fc
Matthew Forrester <matthew.forrester@speechmarks.com>
2021-04-16 22:40:38 +0100

    feat: enable recursion

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+function trim(s) {
+    while (s.substr(0, 1) == " ") { s = s.substr(1); }
+    while (s.substr(-1) == " ") { s = s.substr(0, s.length - 1); }
+    return s
+}
+
+function decode(infoString) {
+    if (infoString.length == 0) { return {}; }
+    if (infoString.indexOf(" ") == -1) {
+        return { lang: infoString, meta: null };
+    }
+    return {
+        lang: infoString.substr(0, infoString.indexOf(" ") + 0),
+        meta: trim(infoString.substr(infoString.indexOf(" ")))
+    };
+}
+
+function getParts(meta, separator) {
+    const separatorPos = meta.indexOf(separator);
+    if (separatorPos == -1) { return {}; }
+    return decode(trim(meta.substr(separatorPos + 1)));
+}
+
+function saneMeta(meta, separator) {
+    if (meta == null) { return null; }
+    if (meta.indexOf(separator) == -1) { return meta; }
+    return meta.substr(0, meta.indexOf(separator));
+}
+
 function copyCodeMetaHashUp(options) {
 
     const separator = (options || {}).separator || '#';
@@ -6,18 +35,18 @@ function copyCodeMetaHashUp(options) {
 
         const children = tree.children.reduce(
             (acc, child) => {
-                if (
-                    (child.type != "code") ||
-                    (child.meta || "").indexOf(separator) == -1
-                ) {
+                if (child.type != "code") {
                     return [...acc, child];
                 }
-                after = child.meta.substr(child.meta.indexOf(separator) + 1);
-                let [whole, lang, _junk, meta] = child.meta.match(
-                    /#\s*([^\s]+)(\s*([^\s].*))?/
-                );
-                meta = meta ? meta : null;
-                return [ ...acc, { ...child, lang, meta }, child ];
+                const newInfo = getParts(child.meta || "", separator);
+                if (!newInfo.lang) {
+                    return [...acc, child];
+                }
+                return [
+                    ...acc,
+                    { ...child, lang: newInfo.lang, meta: newInfo.meta },
+                    { ...child, lang: child.lang, meta: saneMeta(child.meta, separator) }
+                ];
             },
             []
         );
@@ -29,3 +58,17 @@ function copyCodeMetaHashUp(options) {
 }
 
 module.exports = copyCodeMetaHashUp;
+
+// const assert = require('assert');
+// assert.deepEqual(getParts("", "#"), {});
+// assert.deepEqual(getParts("bash", "#"), {});
+// assert.deepEqual(getParts("bash strict", "#"), {});
+// assert.deepEqual(getParts("bash strict #", "#"), {});
+// assert.deepEqual(getParts("bash strict #morebash", "#"), {lang: "morebash", meta: null});
+// assert.deepEqual(getParts("bash strict #morebash   ", "#"), {lang: "morebash", meta: null});
+// assert.deepEqual(getParts("bash strict #morebash   x", "#"), {lang: "morebash", meta: "x"});
+// assert.deepEqual(getParts("bash strict #morebash   x x", "#"), {lang: "morebash", meta: "x x"});
+// assert.deepEqual(getParts("bash strict # morebash", "#"), {lang: "morebash", meta: null});
+// assert.deepEqual(getParts("bash strict # morebash   ", "#"), {lang: "morebash", meta: null});
+// assert.deepEqual(getParts("bash strict # morebash   x", "#"), {lang: "morebash", meta: "x"});
+// assert.deepEqual(getParts("bash strict # morebash   x x", "#"), {lang: "morebash", meta: "x x"});
